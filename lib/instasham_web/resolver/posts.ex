8d37d10bc37fc1e20633198e7fdd0d18794e8e00
Philip Alekseev <phil.alekseev@gmail.com>
2018-02-04 00:21:37 -0500

    Add query to get photo by id

--- lib/instasham_web/resolver/posts.ex
+++ lib/instasham_web/resolver/posts.ex
@@ -2,4 +2,8 @@ defmodule InstashamWeb.Resolvers.Posts do
   def photos(_, _, _) do
     {:ok, Instasham.Posts.list_photos}
   end
+
+  def photo(_, %{id: id}, _) do
+    {:ok, Instasham.Posts.get_photo!(id)}
+  end
 end
