ba8f3c9f32503721ddc101c55f55b23e1b3ed4cd
Philip Alekseev <phil.alekseev@gmail.com>
2018-02-03 22:48:17 -0500

    Add mock posts to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,25 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Instasham.Repo.insert!(%Instasham.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Instasham.{Posts, Repo}
+
+photos_list = [
+  "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?h=350&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/271955/pexels-photo-271955.jpeg?h=350&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/257532/pexels-photo-257532.jpeg?h=350&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?h=350&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/96938/pexels-photo-96938.jpeg?h=350&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/290164/pexels-photo-290164.jpeg?h=350&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?h=350&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg?h=350&auto=compress&cs=tinysrgb",
+  "https://images.pexels.com/photos/209037/pexels-photo-209037.jpeg?h=350&auto=compress&cs=tinysrgb"
+]
+photos_last_index = length(photos_list) - 1
+
+for i <- 0..photos_last_index do
+  photo = %{
+    image_url: Enum.at(photos_list, i),
+    caption: Faker.Lorem.Shakespeare.hamlet
+  }
+
+  %Posts.Photo{}
+  |> Posts.Photo.changeset(photo)
+  |> Repo.insert!
+end
